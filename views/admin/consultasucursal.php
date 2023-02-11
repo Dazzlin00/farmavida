9e4b578345c538e352f98f75773a5fac07b45997
Omerly <omerly.22.dlaml@gmail.com>
2023-02-11 16:12:03 -0400

    otros cambios

--- views/admin/consultasucursal.php
+++ views/admin/consultasucursal.php
@@ -67,27 +67,8 @@
                     <?php echo $sucursals->direccion ?>
                   </td>
                   <td><a class="btn btn-success" href="<?php echo constant('URL') . 'sucursals/verSucursal/' . $sucursals->codSucursal; ?>">Actualizar</a></td>
-                  <td><a class="btn btn-danger text-white" data-toggle="modal" data-target="#confirmModal">Eliminar</a></td>
-                                <!-- Modal de confirmación -->
-                                <div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog">
-                                          <div class="modal-content">
-                                            <div class="modal-header">
-                                              <h5 class="modal-title" id="exampleModalLabel">Confirmación</h5>
-                                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                              </button>
-                                            </div>
-                                            <div class="modal-body">
-                                              ¿Está seguro de que desea eliminar esta sucursal?
-                                            </div>
-                                            <div class="modal-footer">
-                                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                                              <a class="btn btn-danger" href="<?php echo constant('URL') . 'sucursals/eliminar/' . $sucursals->codSucursal; ?>">Eliminar</a>
-                                            </div>
-                                          </div>
-                                        </div>
-                                      </div>
+                  <td><a class="btn btn-danger" href="<?php echo constant('URL') . 'sucursals/eliminar/' . $sucursals->codSucursal; ?>">Eliminar</a></td>
+                            
                 
 
                 </tr>
